997f4b8b1d9f53aa19ddad5a41019cb6dcd9640d
Joseguera <joseguera@uclaextension.edu>
2021-01-19 11:46:47 -0800

    added db url

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
 
+
+// db connection
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
