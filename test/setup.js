01acd369bc0d1fc28f88fa6c909f599e5070ed70
Joseguera <joseguera@uclaextension.edu>
2021-01-20 10:33:43 -0800

    edited db

--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,7 @@ process.env.JWT_SECRET = 'test-jwt-secret'
 process.env.JWT_EXPIRY = '3m'
 
 require('dotenv').config()
-
+// connect to test db
 process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
   || "postgresql://dunder-mifflin@localhost/spaced-repetition-test"
 
