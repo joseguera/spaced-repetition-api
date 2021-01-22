ef03f609f503505ae4750b7aa5fbfe7873417bc0
Joseguera <joseguera@uclaextension.edu>
2021-01-21 21:13:26 -0800

    updated server 1

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   API_TOKEN: process.env.API_TOKEN,
   DATABASE_URL: process.env.DATABASE_URL || 'postgres://localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  // JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
